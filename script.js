5cffdccb3717e56c8e61526cb9d64af2f4a9d43d
Vedamruta <vedamruta2222@gmail.com>
2023-10-16 15:10:56 +0530

    pre-final change

--- script.js
+++ script.js
@@ -74,32 +74,13 @@ function displayResult(user, computer) {
 function user_logic(event) {
     // console.log(event.target);
     
-    if (count == 5) {
-        // result.innerHTML = " GAME OVER";
-        input_buttons.forEach((icon) => {
-            icon.removeEventListener('click', user_logic);
-        });
-        // console.log(count);
-        count = 0;
-        // console.log(count);
-    setTimeout(displayResult(user_score, computer_score), 500);
+    if(count > 4) return;
 
-    }
-
-    // if(count == 5) {
-    //     result.innerHTML += " GAME OVER";
-    //     input_buttons.forEach((icon) => {
-    //         event.preventDefault();
-    //     });
-    //     // event.preventDefault();
-    //     console.log(count);
-    //     // count = 0;
-    // }
 
     // user part starts here
     let user;
     if (user_click.classList.contains(array[2])) {
-        user_click.classList.remove(array[2])
+        user_click.classList.remove(array[2]);
     }
     else if(user_click.classList.contains(array[1])) {
         user_click.classList.remove(array[1]);
@@ -112,17 +93,14 @@ function user_logic(event) {
     if(dataItem === 'scissors') {
         user_click.classList.add(array[2]);
         user = 2;
-        // user_score.innerHTML = text_array[2];
     }
     else if(dataItem === 'rock') {
         user_click.classList.add(array[0]);
         user = 0;
-        // user_text.innerHTML = text_array[0];
     }
     else if(dataItem === 'paper') {
         user_click.classList.add(array[1]);
         user = 1;
-        // user_text.innerHTML = text_array[1];
     }
     // user part ends here
 
@@ -140,15 +118,12 @@ function user_logic(event) {
     }
     if(computer === 0) {
         computer_input.classList.add(array[0]);
-        // computer_text.innerHTML = text_array[0];
     }
     else if(computer === 1) {
         computer_input.classList.add(array[1]);
-        // computer_text.innerHTML = text_array[1];
     }
     else if(computer === 2) {
         computer_input.classList.add(array[2]);
-        // computer_text.innerHTML = text_array[2];
     }
     // computer part ends here
 
@@ -201,8 +176,19 @@ function user_logic(event) {
         }
     }
 
-    console.log(computer_score);
-    console.log(user_score);
+    // console.log(computer_score);
+    // console.log(user_score);
+
+    if (count === 4) {
+        input_buttons.forEach((icon) => {
+            icon.removeEventListener('click', user_logic);
+        });
+        // console.log(count);
+        count = 0;
+        // console.log(count);
+    setTimeout(displayResult(user_score, computer_score), 500);
+        return;
+    }
 
     count++;
 
@@ -210,5 +196,4 @@ function user_logic(event) {
 
 input_buttons.forEach((icon) => {
     icon.addEventListener('click', user_logic);
-    // setTimeout(displayResult(user, computer), 500);
 });
